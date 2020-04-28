406cd0eb8e60844e12103ba903ac1d3ab962dfa4
Raviv S <raviv.schaffer@gmail.com>
2020-04-28 14:12:04 +0300

    Added input validation and minor fixes in the main loop.

--- cinema.py
+++ cinema.py
@@ -6,35 +6,48 @@ class Movie:
 
     def buy_ticket(self, amount):
         """
-        Calculates the price of all the tickets the user bought.
-        Updates the cost of the ticket accordingly.
+        Calculates the price of all the tickets the user bought and updates the cost of the ticket accordingly.
         :param amount: Number of ticket purchased
         :return: The price of all the tickets
         """
-        # The total price can be reduced to a singular equation
+        # The total price here reduced to a singular equation
         total_amount = amount * self.cost + 2 * sum(range(amount))
         self.cost = self.cost + 2 * amount
         return total_amount
 
 
-movies_catalog = [Movie('Wonder Woman 1984'), Movie('James Bond No Time To Die'), Movie('Black Widow')]
-username = ''
+def main():
+    movies_catalog = [Movie('Wonder Woman 1984'), Movie('James Bond No Time To Die'), Movie('Black Widow')]
+    username = ''
+    cinema_revenue = 0
+    while username != 'quit':
+        username = input('Welcome! What\'s your name? (type "quit" to quit) - ')
+        if username != 'quit':
+            ticket_count = input('Hello {}, How many tickets would you like to purchase: '.format(username))
+            # Checks for valid ticket input
+            try:
+                ticket_count = int(ticket_count)
+            except:
+                print('Invalid amount of tickets')
+            else:
+                if ticket_count > 0:
+                    print('Now pick a movie: ')
+                    for movie in movies_catalog:
+                        print(movie.name + ', Price - {}$'.format(movie.cost))
+                    input_movie = input().lower()
+                    # Checks which movie was chosen and calculates it's price
+                    if input_movie in [movie.name.lower() for movie in movies_catalog]:
+                        for movie in movies_catalog:
+                            if movie.name.lower() == input_movie:
+                                price = movie.buy_ticket(ticket_count)
+                                cinema_revenue += price
+                        print('That will be {}$\r\nGoodbye.'.format(price))
+                    else:
+                        print('Sorry, {} is not in the catalog.'.format(input_movie))
+
+                else:
+                    print('Invalid amount of tickets')
 
 
-while username != 'quit':
-    username = input('Type your name (type "quit" to quit) - ')
-    chosen_movie = ''
-    price = 0
-    if username != 'quit':
-        ticket_count = int(input('Hello {}, how many tickets would you like to purchase: '.format(username)))
-        # To add - check valid input
-        print('Now pick a movie: ')
-        for movie in movies_catalog:
-            print(movie.name + ', Price - {}$'.format(movie.cost))
-        input_movie = input()
-        for movie in movies_catalog:
-            if movie.name == input_movie:
-                chosen_movie = movie
-                movie.buy_ticket(ticket_count)
-        print(chosen_movie)
-        print(movie.cost)
+if __name__ == '__main__':
+    main()
