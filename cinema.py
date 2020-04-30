716b965ea05584fd8412fa6dfd2bdcee1b5b06fa
Raviv S <raviv.schaffer@gmail.com>
2020-04-30 21:05:08 +0300

    Splitted main() to 3 new functions. Removed the built-in index in Movie class and changed it to be a list index

--- cinema.py
+++ cinema.py
@@ -1,8 +1,7 @@
 class Movie:
 
-    def __init__(self, name, index):
+    def __init__(self, name):
         self.name = name
-        self.index = index
         self.cost = 2
 
     def buy_ticket(self, amount):
@@ -25,45 +24,73 @@ def choose_movie(movie_name_ind, catalog):
     :return: Movie object
     """
     for movie in catalog:
-        if (movie.name == movie_name_ind) | (movie.index == movie_name_ind):
+        if (movie.name == movie_name_ind) or (str(catalog.index(movie)) == movie_name_ind):
             return movie
-    # To the reviewer - The same can be done with filter but it's less readable and a bit messier.
-    # return list(filter(lambda movie: (movie.name.lower() == movie_name_ind.lower()) | (movie.index == movie_name_ind),
-    #                   catalog))[0]
+
+
+def pay_movie(ticket_count, catalog):
+    """
+    Takes user input for a movie, checks if it's in the catalog and pay for the tickets.
+    :param ticket_count:
+    :param catalog:
+    :return: ticket's price
+    """
+    print('Now pick a movie: ')
+    for movie in catalog:
+        print('{}. {}, Price - {}$'.format(catalog.index(movie), movie.name, movie.cost))
+    input_movie = choose_movie(input(), catalog)
+    if input_movie is not None:
+        price = input_movie.buy_ticket(ticket_count)
+        print('That will be {}$\r\nGoodbye.'.format(price))
+    else:
+        price = 0
+        print('Sorry, that movie is not in the catalog.')
+    return price
+
+
+def ticket_cnt_inp():
+    """
+    Takes user input for amount of tickets and checks if it is valid. If it ain't valid, returns 0.
+    :return: ticket count (or 0 if invalid input was given)
+    """
+    ticket_count = input('How many tickets would you like to purchase? ')
+    try:
+        ticket_count = int(ticket_count)
+    except:
+        print('Invalid amount of tickets')
+        return 0
+    else:
+        if ticket_count > 0:
+            return ticket_count
+        else:
+            print('Invalid amount of tickets.')
+            return 0
+
+
+def sum_stats(catalog, revenue):
+    """
+    Show summarize statistics of the lst purchases - Amount of tickets bought for each movie and the cinema revenue
+    :param catalog:
+    :param revenue:
+    :return: None
+    """
+    for movie in catalog:
+        print('{} ticket(s) were bought for {}.'.format(int(movie.cost / 2 - 1), movie.name))
+    print('Overall cinema revenue: {}$'.format(revenue))
 
 
 def main():
-    movies_catalog = [Movie('Wonder Woman 1984', '0'), Movie('James Bond No Time To Die', '1'),
-                      Movie('Black Widow', '2')]
+    movies_catalog = [Movie('Wonder Woman 1984'), Movie('James Bond No Time To Die'), Movie('Black Widow')]
     username = ''
     cinema_revenue = 0
     while username != 'quit':
         username = input('Welcome! What\'s your name? (type "quit" to quit) - ')
         if username != 'quit':
-            ticket_count = input('Hello {}, How many tickets would you like to purchase: '.format(username))
-            # Checks for valid ticket input
-            try:
-                ticket_count = int(ticket_count)
-            except:
-                print('Invalid amount of tickets')
-            else:
-                if ticket_count > 0:
-                    print('Now pick a movie: ')
-                    for movie in movies_catalog:
-                        print('{}. {}, Price - {}$'.format(movie.index, movie.name, movie.cost))
-                    input_movie = choose_movie(input(), movies_catalog)
-                    if input_movie is not None:
-                        price = input_movie.buy_ticket(ticket_count)
-                        cinema_revenue += price
-                        print('That will be {}$\r\nGoodbye.'.format(price))
-                    else:
-                        print('Sorry, that movie is not in the catalog.')
-                else:
-                    print('Invalid amount of tickets.')
+            ticket_count = ticket_cnt_inp()
+            if ticket_count != 0:
+                cinema_revenue += pay_movie(ticket_count, movies_catalog)
         else:
-            for movie in movies_catalog:
-                print('{} ticket(s) were bought for {}.'.format(int(movie.cost / 2 - 1), movie.name))
-            print('Overall cinema revenue: {}$'.format(cinema_revenue))
+            sum_stats(movies_catalog, cinema_revenue)
 
 
 if __name__ == '__main__':
